1f81b2024e634de4184c4e6ef833329a143f0b0d
Anthony Perriolat <anthonyperriolat@iMac-de-Anthony.local>
2021-03-26 14:56:20 +0100

    Réalisation du Feed #4

--- js/script.js
+++ js/script.js
@@ -3,24 +3,20 @@ $(function(){
         url: 'http://api.icndb.com/jokes/random/5', 
         method: 'GET', 
         dataType : 'json' 
-    })
-
-    .done(function(reponsechuck){
-        
-
-        console.log(reponsechuck.value[0].id); 
-        // let data = JSON.stringify(reponsechuck); // enlever le sting 
-        // $("section article").append(data['value']);
-        $( "section" ).each(function( index ) {
-            console.log( index + ": " + $( this ).text() );
-          });
-
+    }).done(function(reponsechuck){    
+        reponsechuck.value.forEach(element =>  $("section").append('<h1>Auteur : CHUCK NORRIS</h1>' + '<p>' 
+        + element.joke + '</p>'
+        + '<form action="" method="get"><input type="text"><button>Commenter</button></form>'
+        ));
+       // OU 
+        /* for (prop in  reponsechuck.value) {
+            $("section").append('<p>' + reponsechuck.value[prop].joke + '</p>');
+        }*/
     })
     .fail(function(erreur){
-        alert("La requete a échoué" + JSON.stringify(erreur));
+        alert("La requete a échoué, Chuk Norris l'as cassé" + JSON.stringify(erreur));
     })
     .always(function(){
-        alert("Requête effectuée");
+        alert("Requête effectuée : Chuck Norris n'attend pas c'est le temps qu'il l'attend");
     });
-    reponsechuk.value.forEach(function(reponsechuck)); // for each n'exite pas jQ
 });
