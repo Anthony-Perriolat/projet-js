00631a33cebc2b35518c5c1056d91ded57e26fcd
Perriolat <anthony.perriolat@le-campus-numerique.fr>
2021-03-29 16:28:53 +0200

    Fin jour

--- js/script.js
+++ js/script.js
@@ -1,41 +1,51 @@
-$(function(){
-    $.ajax({
-        url: 'http://api.icndb.com/jokes/random/5', 
-        method: 'GET', 
-        dataType : 'json' 
-    }).done(function(reponsechuck){    
-        reponsechuck.value.forEach(element =>  $("section").append('<h1>Auteur : CHUCK NORRIS</h1>' + '<p>' 
-        + element.joke + '</p>'
-        + '<form action="" method="get"><input type="text"><button>Commenter</button></form>'
-        ));
-       // OU 
-        /* for (prop in  reponsechuck.value) {
-            $("section").append('<p>' + reponsechuck.value[prop].joke + '</p>');
-        }*/
-    })
-    .fail(function(erreur){
-        alert("La requete a échoué, Chuk Norris l'as cassé" + JSON.stringify(erreur));
-    })
-    .always(function(){
-        alert("Requête effectuée : Chuck Norris n'attend pas c'est le temps qu'il l'attend");
+$(function () {
+  $.ajax({
+    url: 'http://api.icndb.com/jokes/random/5',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function (reponsechuck) {
+    reponsechuck.value.forEach(element => $("section").append('<h1>Auteur : CHUCK NORRIS</h1>' + '<p>'
+      + element.joke + '</p>'
+    ));
+    // OU 
+    /* for (prop in  reponsechuck.value) {
+        $("section").append('<p>' + reponsechuck.value[prop].joke + '</p>');
+    }*/
+  })
+    .fail(function (erreur) {
+      alert("La requete a échoué, Chuk Norris l'as cassé" + JSON.stringify(erreur));
     });
 
-    // setting carrousel https://www.jqueryscript.net/slider/Responsive-Flexible-Mobile-Touch-Slider-Swiper.html
+  // setting carrousel https://www.jqueryscript.net/slider/Responsive-Flexible-Mobile-Touch-Slider-Swiper.html
 
-    var swiper = new Swiper('.swiper-container', {
-                pagination: {
-                  el: '.swiper-pagination',
-                  type: 'progressbar',
-                },
-                navigation: {
+  var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+    },
+    navigation: {
 
-                  nextEl: '.swiper-button-next',
-                  prevEl: '.swiper-button-prev',
-                },
-                scrollbar: {
-                  el: '.swiper-scrollbar',
-                  hide: true,
-                },
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: true,
+    },
 
-            });
+  });
+
+  let isOpen = false;
+  function menuDropdown() {
+    if (isOpen) {
+      document.getElementById('myDropdown').style.display = "none";
+    }
+    else {
+      document.getElementById('myDropdown').style.display = "flex";
+    }
+    isOpen = !isOpen;
+  }
+
+
+
 });
