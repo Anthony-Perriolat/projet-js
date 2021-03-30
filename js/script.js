3d974b8f9d9a9b96876476d8eda29cf9a2a81872
Anthony-Perriolat <pro.aperriolat@gmail.com>
2021-03-30 10:38:16 +0200

    feature : dropdown

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-$(function () {
-  $.ajax({
+$(document).ready(function () {
+  let data = $.ajax({
     url: 'http://api.icndb.com/jokes/random/5',
     method: 'GET',
     dataType: 'json'
@@ -16,6 +16,12 @@ $(function () {
       alert("La requete a échoué, Chuk Norris l'as cassé" + JSON.stringify(erreur));
     });
 
+    data;
+
+    $('#refresh').click(function () {
+      data;
+    })
+    
   // setting carrousel https://www.jqueryscript.net/slider/Responsive-Flexible-Mobile-Touch-Slider-Swiper.html
 
   var swiper = new Swiper('.swiper-container', {
@@ -34,18 +40,22 @@ $(function () {
     },
 
   });
-
-  let isOpen = false;
-  function menuDropdown() {
-    if (isOpen) {
-      document.getElementById('myDropdown').style.display = "none";
-    }
-    else {
-      document.getElementById('myDropdown').style.display = "flex";
-    }
-    isOpen = !isOpen;
-  }
+  // menu deroulant 
+  $('#buttonDropdown').click(function () {
+    menuDropdown()
+  });
 
 
 
 });
+
+let isOpen = false;
+function menuDropdown() {
+  if (isOpen) {
+    document.getElementById('myDropdown').style.display = "none";
+  }
+  else {
+    document.getElementById('myDropdown').style.display = "flex";
+  }
+  isOpen = !isOpen;
+}
